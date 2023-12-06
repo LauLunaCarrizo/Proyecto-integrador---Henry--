085dae27ed55719d75ff582e056270d6d2992f55
lauu <lautaropp03@outlook.com>
2023-12-06 11:05:30 -0300

    testing

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,24 +1,5 @@
-const express = require('express');
-const Router = require("./routes/index")
-const server = express();
+const server = require("./app")
 const PORT = 3001;
-
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
-
-server.use(express.json())
-server.use("/rickandmorty",Router)
 
 server.listen(PORT, () => {
    console.log('Server raised in port: ' + PORT);
