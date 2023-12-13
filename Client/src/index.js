5728312968ec86f6780e240b8637dc387d1e32b7
lauu <lautaropp03@outlook.com>
2023-12-13 10:29:35 -0300

    creacion y sincronizacion a base de datos

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import store from './redux/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
     <App />  
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,14 @@
 const server = require("./app")
 const PORT = 3001;
+const {conn} = require("./DB_connection");
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
+
+
+server.listen(PORT, async() => {
+   try {
+      await conn.sync({force: true})
+      console.log('Server raised in port: ' + PORT);
+   } catch (error) {
+      console.log(error.message);
+   }
 });
